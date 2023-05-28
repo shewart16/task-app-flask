56bb2cd5b39f3687af1a3d59f9205851f03e1880
shewart16 <pipe161102@outlook.com>
2023-05-28 17:30:29 -0500

    Init app

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    completed = db.Column(db.Boolean)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def index():
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
+
+
+@app.route("/create-task/", methods=["POST"])
+def new():
+    new_task = Task(content=request.form["content"], completed=False)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
